043b6518766ec1e115e0d013a4a535caa3d10511
rayrzy <vincentrayananda@gmail.com>
2022-10-31 21:08:19 +0700

    surya: QASSA-ify
    Signed-off-by: rayrzy <vincentrayananda@gmail.com>
    Change-Id: I4033bb0d61b6d823e161a1b47ded4e372bf36d2d

--- qassa_surya.mk
+++ qassa_surya.mk
@@ -11,11 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common keepQASSA stuff.
+$(call inherit-product, vendor/qassa/config/common_full_phone.mk)
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+USE_PIXEL_CHARGING := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := qassa_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
